c113f6359a6bbd3e3a05a66123c04663f8cb6fa9
Rubens <rubens@prosol.com.br>
2019-04-03 14:00:31 -0300

    SmartCall - Inicio do projeto

---
+++ SmartCall-JAVA/src/smartcall/java/Database/c_ConexaoDB.java
@@ -0,0 +1,25 @@
+package smartcall.java.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class c_ConexaoDB {
+    
+    public Connection ConexaoAvulsa(){
+        String url = "jdbc:postgresql://localhost:5432/SmartCall";
+        String usuario = "postgres";
+        String senha = "123456";
+
+        try{
+            Class.forName("org.postgresql.Driver").newInstance();
+            Connection conn = DriverManager.getConnection(url, usuario, senha);
+          return conn;
+          
+        }catch(ClassNotFoundException | IllegalAccessException | InstantiationException | SQLException e){
+            System.out.println("Problemas ao tentar conectar com o banco de dados: " + e);
+            return null;
+        }
+    }
+}
+
