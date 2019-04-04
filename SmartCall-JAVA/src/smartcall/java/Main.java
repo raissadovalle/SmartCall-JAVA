78dc9d99f26e1428cff214ef8c75ef45fefd4ea3
Rubens <rubens@prosol.com.br>
2019-04-04 17:33:58 -0300

    Ajustes

--- SmartCall-JAVA/src/smartcall/java/Main.java
+++ SmartCall-JAVA/src/smartcall/java/Main.java
@@ -3,8 +3,13 @@ package smartcall.java;
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,11 +21,13 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         
-        Parent root = FXMLLoader.load(getClass().getResource("w_MainWindow.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("w_MainWindow.fxml"));              
         
         stage.setTitle("SmartCall");
         stage.setMaximized(true);
         stage.setScene(new Scene(root));
         stage.show();
-    }    
+        
+    } 
+
 }
